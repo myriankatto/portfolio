123335455ed808bba4aaf679db3b376265cbb92d
Myrian Katto <myriankatto@gmail.com>
2020-04-19 16:41:18 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Myrian's Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
